044017482f5da88cac03c0a532fb3ddae760dfbf
Santiago B <129016265+SmaugLoke@users.noreply.github.com>
2023-11-14 16:19:02 -0500

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Proyecto_PLTcurso.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
