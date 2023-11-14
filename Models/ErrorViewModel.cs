ba10470d1ebf77bc7d6e49e178ad870e050b6a56
DragonLoke <santiagobeltranmontenegro@gmail.com>
2023-11-14 15:25:33 -0500

    Proyecto Integrado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Proyecto_PLTcurso.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
